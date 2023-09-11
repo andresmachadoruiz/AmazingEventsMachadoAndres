01e0d27cdc8fae7e79737e340ff1edb0b44d26b4
andresmachadoruiz <andresmachiz1@gmail.com>
2023-09-11 06:42:51 -0500

    added checkboxes and filters

--- JS/index.js
+++ JS/index.js
@@ -1,29 +1,82 @@
-let eventsList = data.events
+let eventsList = data.events;
+let listCards = eventsList;
+const containerChecks = document.getElementById("checkContainer")
+const contenedorCartas = document.getElementById("cardsContain")
 
 function createCard(indEvents) {
- return   `<div class="card1 col">
- <img src="${indEvents.image}" class="card-img-top" alt="homebanner2" style="width: 18rem">
- <div class="card-body">
-   <h2 class="card-title">${indEvents.name}</h2>
-   <p class="card-text">${indEvents.description}</p>
-   <a href="./pages/details.html" class="btn btn-primary">See More Details</a>
- </div>
-</div> `
+    return `<div class="card m-3" style="width: 18rem;">
+    <img src="${indEvents.image}" class="card-img-top cardimg" alt="homebanner2" style="width: 18rem">
+    <div class="card-body">
+      <h2 class="card-title">${indEvents.name}</h2>
+      <p class="card-text">${indEvents.description}</p>
+      <a href="../pages/details.html?parametro=${indEvents._id}" class="btn btn-primary">See More Details</a>
+    </div>
+   </div> `;
 }
 
-function newCards(listCards) {
-let template = "";
-for (const card of listCards) {
-    template += createCard(card);
-}
-return template;
-}
-
-function printSite(template, id){
-
-let container = document.getElementById(id);
-container.innerHTML = template;
+function newCards(listCards, container){
+    let template = "";
+    for (const indEvents of listCards) {
+            template += createCard(indEvents);
+    }
+    container.innerHTML = template;
 }
 
-const template = newCards(eventsList);
-printSite(template, "cardsContain");
+newCards(listCards, contenedorCartas)
+
+
+
+//Checkboxes 
+
+const categories = [];
+
+for (const iterator of listCards) {
+  const keyCategory = iterator.category;
+  if (!categories.includes(keyCategory)) {
+    categories.push(keyCategory);
+  }
+}
+
+function checkBox(categ) {
+  let template = "";
+  for (let i = 0; i < categ.length; i++) {
+    template += `<div class="form-check form-check-inline">
+    <input class="form-check-input"  type="checkbox"  id="${[i]}"  value="${categ[i]}">
+    <label class="form-check-label" for="${[i]}">${categ[i]}</label>
+  </div>`;
+  }
+  return template;
+}
+
+function printCheckBox(array, container) {
+  const template = checkBox(array);
+  container.innerHTML = template;
+}
+
+printCheckBox(categories, checkContainer);
+
+
+//Checkboxes container structure
+
+
+//Filtering events
+
+//
+
+checkContainer.addEventListener("change", () => {
+const returnFilter = filterCheck(eventsList);
+printSite(returnFilter, contenedorCartas)
+
+})
+
+function filterCheck(array) {
+  const nodeList = document.querySelectorAll("input[type='checkbox']:checked");
+
+  const checkBoxArray = Array.from(nodeList);
+  const valueArray = checkBoxArray.map((input) => input.value);
+  const filterArray = array.filter((objeto) =>
+    valueArray.includes(objeto.category)
+  );
+
+  return filterArray;
+}
--- JS/pastevents.js
+++ JS/pastevents.js
@@ -2,22 +2,20 @@ let eventsList = data.events;
 const currentDate = parseInt(data.currentDate.split("-").join(""));
 
 function createCard(indEvents) {
-    return `
-    <div class="card1 col">
-        <img src="${indEvents.image}" class="card-img-top" alt="homebanner2" style="width: 18rem">
-        <div class="card-body">
-            <h2 class="card-title">${indEvents.name}</h2>
-            <p class="card-text">${indEvents.description}</p>
-            <a href="./pages/details.html" class="btn btn-primary">See More Details</a>
-        </div>
-    </div>`;
+    return `<div class="card m-3" style="width: 18rem;">
+    <img src="${indEvents.image}" class="card-img-top cardimg" alt="homebanner2" style="width: 18rem">
+    <div class="card-body">
+      <h2 class="card-title">${indEvents.name}</h2>
+      <p class="card-text">${indEvents.description}</p>
+      <a href="../pages/details.html?parametro=${indEvents._id}" class="btn btn-primary">See More Details</a>
+    </div>
+   </div> `;
 }
 
 function newCards(listCards) {
     let template = "";
     for (const indEvents of listCards) {
         let dateToNumber = parseInt(indEvents.date.split("-").join(""));
-        console.log(dateToNumber);
         if (dateToNumber < currentDate) {
             template += createCard(indEvents);
         }
@@ -25,10 +23,85 @@ function newCards(listCards) {
     return template;
 }
 
-function printSite(id) {
-    const template = newCards(eventsList);
-    let container = document.getElementById(id);
-    container.innerHTML = template;
-}
+function printSite(template, id) {
+    const container = document.getElementById(id);
 
-printSite("cardsContain");
+    // Check if the container element was found
+    if (container) {
+        // Access and manipulate the container's properties, like innerHTML
+        container.innerHTML = template;
+    } else {
+        console.error("Container element not found");
+    }
+}  
+    const cardsTemplate = newCards(eventsList);
+    printSite(cardsTemplate, "cardsContain");
+
+
+//Checkboxes 
+
+function createChecks(checksCats) {
+    return `
+      <div class="form-check form-check-inline">
+        <input class="form-check-input"  type="checkbox"  id="${checksCats}"  value="${checksCats}">
+        <label class="form-check-label" for="${checksCats}">${checksCats}</label>
+      </div>  
+    `
+  }
+  
+  // checks creation
+  
+  
+  function filteredChecks (eventsList, category) {
+    let filteringArray = [];
+    for (const card of eventsList) {
+      if (card.category == checkBox.value) {
+        filteringArray.push(card);
+      }
+    }
+    const filteredArray = filteringArray.filter(filteredChecks);
+    return filteringArray
+  }
+  
+  
+  
+  function newCheckBox(eventsList) {
+    let template = "";
+    for (const event of eventsList) {
+      let aux = event.category;
+        console.log(aux)
+        template += createChecks(aux);
+    }
+   return template
+    } 
+  const checkbox = newCheckBox (eventsList);
+  
+  
+  //Print checks array
+  printSite(checkbox, "checkContainer");
+  
+  //remove repeat arrays
+  const arrayChecks = eventsList.map(event => event.category);
+  
+  const uniqueSet = new Set(arrayChecks);
+  const uniqueArray = [...uniqueSet];
+  
+  console.log(uniqueArray)
+  
+  // Define a Set to store unique categories
+  const uniqueCategories = new Set();
+  
+  // Iterate through events and add unique categories to the Set
+  for (const event of eventsList) {
+    uniqueCategories.add(event.category);
+  }
+  
+  // Create checkboxes based on unique categories
+  let checkboxesHTML = "";
+  for (const category of uniqueCategories) {
+    checkboxesHTML += createChecks(category);
+  }
+  
+  // Print the checkboxes
+  printSite(checkboxesHTML, "checkContainer");
+
